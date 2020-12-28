f9d35322a49888db43c2a61828c0cad0124ca17a
willian <willian_98@outlook.com>
2020-12-28 17:12:20 -0400

    remove requests atribute and added baseUrl

--- src/templates/new-project.js
+++ src/templates/new-project.js
@@ -2,9 +2,9 @@ module.exports = function (args) {
   const template = {
     name: args.name || "New project",
     description: args.description || "Imagine a beautiful description here :)",
+    baseUrl: args.baseUrl || "http://localhost:8080",
     authType: args.auth_type || null,
     authKey: args.auth_key || null,
-    requests: [],
   };
 
   const json = JSON.stringify(template, null, "  ");
