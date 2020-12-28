fb903b9533d080f9e57c25319d296ae6cd062df1
willian <willian_98@outlook.com>
2020-12-28 17:09:31 -0400

    using async/await

--- src/features/create-project.js
+++ src/features/create-project.js
@@ -4,20 +4,23 @@ const pathLib = require("path");
 const newProjectTemplate = require("../templates/new-project.js");
 const { saveFile, mkDir } = require("../utils/fileHelper");
 
-module.exports = function (args) {
+module.exports = async function (args) {
+  if (!args.path) {
+    throw new Error("--path option is required!");
+  }
+
   const newProjectFile = newProjectTemplate(args);
 
   if (args.name) {
-    mkDir(args.path, args.name).then(function () {
-      let path = pathLib.join(args.path, args.name);
-      saveFile(path, "config.json", newProjectFile);
-    });
-    return;
+    await mkDir(args.path, args.name);
+
+    let path = pathLib.join(args.path, args.name);
+    return await saveFile(path, "config.json", newProjectFile);
   }
 
   const name = "new_project";
-  mkDir(args.path, name).then(function () {
-    let path = pathLib.join(args.path, name);
-    saveFile(path, "config.json", newProjectFile);
-  });
+  await mkDir(args.path, name);
+
+  let path = pathLib.join(args.path, name);
+  return await saveFile(path, "config.json", newProjectFile);
 };
