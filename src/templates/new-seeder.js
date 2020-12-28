7c6a93110290a2b7123cc5db776d00f87ceddcc8
willian <willian_98@outlook.com>
2020-12-28 17:12:35 -0400

    created new-seeder template

---
+++ src/templates/new-seeder.js
@@ -0,0 +1,43 @@
+const pathLib = require("path");
+const { getJson } = require("../utils/fileHelper");
+
+module.exports = function (args) {
+  const path = pathLib.join(args.project_path, "config.json");
+  const config = getJson(path);
+
+  let fullUrl = config.baseUrl;
+
+  if (args.url) {
+    fullUrl = `${fullUrl}${args.url}`;
+  }
+
+  const template = {
+    _help: `Use the bodies array to put the body of your seeder. Each body inserted will be a request on that route`,
+    _active: true,
+    name: args.name || "New seeder",
+    description: args.description || "Imagine a beautiful description here :)",
+    method: args.method || "POST",
+    url: fullUrl,
+    requests: [
+      {
+        _active: false,
+        _identifier: "request1",
+        data: {
+          some_atrubute: "some_value",
+          some_atrubute2: "some_value2",
+        },
+      },
+      {
+        _active: false,
+        _identifier: "request2",
+        data: {
+          some_atrubute: "some_value",
+          some_atrubute2: "some_value2",
+        },
+      },
+    ],
+  };
+
+  const json = JSON.stringify(template, null, "  ");
+  return json;
+};
